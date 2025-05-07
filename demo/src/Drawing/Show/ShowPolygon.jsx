56112ceadba0353ed3594b6462ba2248ed05e606
Vignesh-Developer002 <vigneshcoder002@gmail.com>
2025-05-07 11:33:59 +0530

    fourth commit

--- demo/src/Drawing/Show/ShowPolygon.jsx
+++ demo/src/Drawing/Show/ShowPolygon.jsx
@@ -4,32 +4,35 @@ import { Line } from "react-konva";
 import { globalStore } from "../../StoreContext/StoreContext";
 
 const ShowPolygon = () => {
-  const { handleTransformetMouseDown, drawPolygon } = useContext(globalStore);
+  const { handleTransformetMouseDown, drawPolygon,setDrawPolygon } = useContext(globalStore);
   //getting the local storage data
   // let res = JSON.parse(localStorage.getItem("polygon"))
   function handlePolygonDrag(e, id) {
     let x = e.target.x();
     let y = e.target.y();
-    let result = res.map((p) => (p.id === id ? { ...p, x: x, y: y } : p));
-    localStorage.setItem("polygon", JSON.stringify(result));
+    // let result = res.map((p) => (p.id === id ? { ...p, x: x, y: y } : p));
+    setDrawPolygon((prev)=>(prev.map((p) => (p.id === id ? { ...p, x: x, y: y } : p))))
+    // localStorage.setItem("polygon", JSON.stringify(result));
   }
 
-  if (!localStorage.getItem("polygon")) {
-    useEffect(() => {
-      localStorage.setItem("polygon", JSON.stringify(polygon));
-    }, []);
-  }
+  // if (!localStorage.getItem("polygon")) {
+  //   useEffect(() => {
+  //     localStorage.setItem("polygon", JSON.stringify(polygon));
+  //   }, []);
+  // }
 
   //function for handletransformEnd i.e) rotation
   function handletransformEnd(e, id, name) {
     let x = e.target.x();
     let y = e.target.y();
     let rotate = e.target.rotation();
-    let localData = JSON.parse(localStorage.getItem(name));
-    let res = localData.map((d) =>
-      d.id === id ? { ...d, x: x, y: y, rotation: rotate } : d
-    );
-    localStorage.setItem(name, JSON.stringify(res));
+    // let localData = JSON.parse(localStorage.getItem(name));
+    // let res = localData.map((d) =>
+    //   d.id === id ? { ...d, x: x, y: y, rotation: rotate } : d
+    // );
+    setDrawPolygon((prev)=>(prev.map((d) =>
+      d.id === id ? { ...d, x: x, y: y, rotation: rotate } : d)))
+    // localStorage.setItem(name, JSON.stringify(res));
   }
   return (
     <>
@@ -46,9 +49,9 @@ const ShowPolygon = () => {
             closed={p.closed}
             draggable={true}
             rotation={p.rotation || 0}
-            // onDragEnd={(e) => handlePolygonDrag(e, p.id)}
-            // onTransformEnd={(e) => handletransformEnd(e, p.id, p.name)}
-            // onMouseDown={(e) => handleTransformetMouseDown(e, p.id, p.name)}
+            onDragEnd={(e) => handlePolygonDrag(e, p.id)}
+            onTransformEnd={(e) => handletransformEnd(e, p.id, p.name)}
+            onMouseDown={(e) => handleTransformetMouseDown(e, p.id, p.name)}
           />
         ))}
     </>
